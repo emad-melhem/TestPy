1906d7682bcd63c679c842dbe30e7cf5aca2e0f5
Imad Mohamad <yehya.mh@live.com>
2024-04-29 21:24:35 +0200

    delete some codes

--- Main.py
+++ Main.py
@@ -2,31 +2,6 @@ from Game import Game
 import math
 
 
-    
-
-circle=chr(9679)
-space= " "
-x=9
-y=5
-z=0
-line={}
-charnum=0
-while charnum < x:
-    line.update({str(charnum): space})
-    charnum +=1
-    
-while z<y:
-    line[str(math.floor(x/2))]=circle
-    line[str(math.floor(x/2)-z)]=circle
-    line[str(math.floor(x/2)+z)]= circle
-    txt= ''
-    for key in line.keys():
-        txt+=line[key]
-    print(space*4 + txt)
-    z +=1
-
-
-
 game = Game()
 winNum=0 # the number of wins
 for x in game._winList: # Detect the number from game list
@@ -35,6 +10,6 @@ for x in game._winList: # Detect the number from game list
 
 print("The final score of the game :")
 if winNum >1:
-    print(f"Congratulations!!\n You won {winNum} times.")
+    print(f"Congratulations!!\nYou won {winNum} times.")
 else:
     print(f"Good luck!!\n You lost {3 - winNum} times.")
